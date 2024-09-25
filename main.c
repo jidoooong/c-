71123ddfd2c9e4b25f8edced047ec632fe0b9e41
jidoooong <kje7684@naver.com>
2024-09-25 13:58:10 +0900

    week_03_1번

--- main.c
+++ main.c
@@ -4,13 +4,16 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int a, b;//정수 변수 선언
+	int input_int;
+	float input_float;
 	
-	a=10;
-	b=20;
+	printf("enter an integer : ");
+	scanf("%d", &input_int);
 	
-		printf("Hello, Sookmyung Electronics!\n");
-		
-	printf("%d + %d = %d\n", a, b, a+b);
+	printf("enter a float : ");
+	scanf("%f", &input_float);
+	
+	printf("integer : %d, float : %f\n", input_int, input_float);
+	
 	return 0;
 }
