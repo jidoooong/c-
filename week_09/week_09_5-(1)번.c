6f7cea3f4049d4fdf6229bc7e54a8c752009a7bd
jidoooong <kje7684@naver.com>
2024-11-01 21:46:23 +0900

    week_09_5번

---
+++ week_09/week_09_5-(1)번.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int all_files;
+
+extern void sub();
+
+int  main(void)
+{
+	sub();
+	printf("%d\n", all_files);
+	return 0;
+}
+
+extern int all_files;
+void sub(void)
+{
+	all_files = 10;
+ } 
